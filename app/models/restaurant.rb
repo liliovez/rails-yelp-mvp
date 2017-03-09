4f1f4380ea60c4350427799824148ffa9ceb03e8
Liliya Ovezmuradova <liliya.ovezmuradova@hec.edu>
2017-03-09 09:31:10 +0100

    style added

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,7 +1,6 @@
 class Restaurant < ApplicationRecord
   has_many :reviews, dependent: :destroy
 
-  validates :phone_number, presence: true
   validates :name, uniqueness: true, presence: true
   validates :address, presence: true
   validates :category, inclusion: { in:[ "chinese", "italian", "japanese", "french", "belgian"], allow_nil: false }
