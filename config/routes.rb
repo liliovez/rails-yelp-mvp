ba2cb95adffb988698aab7b1c7628a51b35bdb89
Liliya Ovezmuradova <liliya.ovezmuradova@hec.edu>
2017-03-10 18:07:04 +0100

    gfef

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   resources :restaurants do
+    get '/delete' to:
     resources :reviews, only: [ :index, :new, :create ]
   end
   resources :reviews, only: [ :show, :edit, :update, :destroy ]
